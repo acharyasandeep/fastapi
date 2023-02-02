ea212404f13257d0f9e3b81b98423929e387861d
Sandeep Acharya <acharyas378@gmail.com>
2023-02-02 15:05:08 +0545

    Handle CORS errors

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@
 #For our project : uvicorn app.main:app --reload
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote
@@ -9,6 +10,16 @@ from .routers import post, user, auth, vote
 
 # models.Base.metadata.create_all(bind=engine) #we can use alembeic, no longer needed to create tables at restart
 app = FastAPI()
+
+origins = ["*"] #list of sites that can access your domain
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
